4cbc7eb2b821bfee4dfa21294d7ae129e687c893
SergeInGithub <nyiriserge@gmail.com>
2024-07-22 20:45:34 +0200

    Task4 - KAN-16: add gear button, gear menu drodwon & add css

--- src/components/Tables/CertificateTable.tsx
+++ src/components/Tables/CertificateTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TCertificate } from '@types';
 import TableHeader from './TableHeader';
 import { TableBody } from './TableBody';
