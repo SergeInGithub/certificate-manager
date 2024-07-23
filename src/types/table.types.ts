f34ab3b9d1ad6e1be616faa02d5b31a38be1d3bc
SergeInGithub <nyiriserge@gmail.com>
2024-07-23 21:07:25 +0200

    Task6 - KAN-18: add lookup modal & ui styles

--- src/types/table.types.ts
+++ src/types/table.types.ts
@@ -40,3 +40,9 @@ export enum CERTIFICATE_TYPE {
   PERMISSION_OF_PRINTING = 'Permission of Printing',
   OHSAS_18001 = 'OHSAS 18001',
 }
+
+export type TSupplier = {
+  supplierName: string;
+  supplierIndex: number;
+  city: string;
+};
